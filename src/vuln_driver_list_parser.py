cb9728daf14bfc0d44fa35f71f22f45dcdd4d6ff
Mehmet Ergene <mehmetergene@gmail.com>
2025-09-13 10:40:44 +0200

    Use StringIO

--- src/vuln_driver_list_parser.py
+++ src/vuln_driver_list_parser.py
@@ -1,11 +1,16 @@
 import pandas as pd
+from io import StringIO
+
+with open("../VulnerableDriverBlockList/SiPolicy_Enforced.xml", "r", encoding="utf-8") as f:
+    xml_string = f.read()
 
 df = pd.read_xml(
-    "../VulnerableDriverBlockList/SiPolicy_Enforced.xml",
+    StringIO(xml_string),
     xpath="SiPolicy:FileRules/",
     namespaces={"SiPolicy": "urn:schemas-microsoft-com:sipolicy"},
     parser="etree"
 )
+
 
 df.fillna("", inplace=True)
 df.to_csv("../msft_vuln_driver_block_list.csv", index=False)
